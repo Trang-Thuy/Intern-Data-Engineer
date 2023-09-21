16f602da5d80b4fffa85c03d2d8428e46c20bf54
Trang-Thuy <82387290+Trang-Thuy@users.noreply.github.com>
2023-09-21 17:43:46 +0700

    upload config file (jupyter notebook and spark)

---
+++ W9-12/config/jupyternotebookconf/jupyter_notebook_config.py
@@ -0,0 +1,9 @@
+
+# u can find this config  file by using this command below (gs)
+# jupyter notebook --generate-config
+
+#add 3 lines in config file 
+c.NotebookApp.port = 9004
+c.NotebookApp.ip = '10.5.94.234' # listen on all IPs 
+c.NotebookApp.allow_origin = '*' #allow all origins
+
